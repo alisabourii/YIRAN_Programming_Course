5ef0ae4fe98aade4d2c13fe48c61868056f0f3ec
Ali SABOURI <72344723+alisabourii@users.noreply.github.com>
2024-11-24 22:24:56 +0300

    Add files via upload

---
+++ var.js
@@ -0,0 +1,31 @@
+/*var aded = 100; // INT
+var meghdar = 1.25; // Float
+var shahr = "Tehran"; // String(str)
+var terjih = true; // Boolean Must True Or Fals
+var personel = {
+        name : "Ali",
+        id : 1234,
+        Sektor: 'Computer'
+        }; // Object
+
+var todayNews = "On Monday, 25 November 2024, NATO Secretary General Mark Rutte will travel to Ankara, Türkiye.Mr Rutte will meet with the President, Mr Recep Tayyip Erdoğan, the Minister of Foreign Affairs, Mr Hakan Fidan, and the Minister of Defence, Mr Yaşar Güler.The Secretary General will engage with representatives from the Turkish defence industry and visit the facilities of the Turkish Airspace Industries (TUSAŞ). He will also lay a wreath at Anıtkabir, Atatürk’s Mausoleum.There will be no media opportunity."
+
+console.log(aded);
+console.log(meghdar);
+console.log(shahr);
+console.log(terjih);
+console.log(personel.name);
+console.log(todayNews);*/
+/* This a shot comment */
+
+var num1 = 20;
+var num2 = 10;
+var total = num1+num2;
+var min = num1-num2;
+var taghsim = num1/num2;
+var zarb = num1*num2;
+
+console.log("+"+total);
+console.log("-"+min);
+console.log("/"+taghsim);
+console.log("*"+zarb);
