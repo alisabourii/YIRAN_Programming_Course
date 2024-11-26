570a42a01dfd22588391172ac1a35c6a74d304bf
Ali SABOURI <72344723+alisabourii@users.noreply.github.com>
2024-11-26 21:25:02 +0300

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,24 @@
+var a = 8;
+var b = 2;
+console.log("a: "+a);
+console.log("b: "+b);
+
+var jame = a+b;
+var tafrigh = a-b;
+var zarb = a*b;
+var taghsim = a/b;
+var mod = a%b;
+
+a++;
+b--;
+
+console.log(jame);
+console.log(tafrigh);
+console.log(zarb);
+console.log(taghsim);
+console.log(mod);
+
+
+console.log("a: "+a);
+console.log("b: "+b);
+
