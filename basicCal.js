5cf9912f5884d35db4e0518132cab47f074c2996
Ali SABOURI <72344723+alisabourii@users.noreply.github.com>
2024-11-29 20:46:46 +0300

    Add files via upload
    Basic Calculator

---
+++ basicCal.js
@@ -0,0 +1,10 @@
+function numFunc(){
+        let num1 = parseInt(document.getElementById("num1").value);
+        let num2 = parseInt(document.getElementById("num2").value);
+        document.getElementById("rsl").value =num1+num2;
+}
+function strFunc(){
+        let num1 = document.getElementById("num1").value;
+        let num2 = document.getElementById("num2").value;
+        document.getElementById("rsl").value = num1+num2;
+}
